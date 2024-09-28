5f9226a797b24f860e333620d13f18bf9063069f
charanpreet01 <charanpreetsinghrajput@gmail.com>
2024-09-29 01:03:45 +0530

    code updated

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ function Sidebar() {
                 <p className='hidden md:block'>Add Items</p>
             </NavLink>
 
-            <NavLink to="/list" className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l">
+            <NavLink to="/" className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l">
                 <img className='w-5 h-5' src={assets.order_icon} alt="" />
                 <p className='hidden md:block'>List Items</p>
             </NavLink>
